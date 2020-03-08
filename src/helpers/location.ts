496409ab0c71d34187b4fa11ae649661a907db77
Nikita Barinov <nikitabarinov2301@gmail.com>
2020-03-08 21:41:25 +0400

    add location helper

---
+++ src/helpers/location.ts
@@ -0,0 +1,41 @@
+export type QueryObject = { [index: number]: any };
+
+export const parseQueryString = (string: string): QueryObject =>
+    string.slice(1).split('&')
+        .map((queryParam) => {
+            const [key, value] = queryParam.split('=');
+
+            return { key, value };
+        })
+        .reduce((query, { key, value }) => ({
+            ...query,
+            [key]: value
+        }), {});
+
+export const getHash = (link: string): string => {
+    const hash = link.split('#')[1];
+
+    if (hash) {
+        return hash.replace('#', '');
+    }
+
+    return '';
+};
+
+export const getHashParam = (link: string, param: string): string | null => {
+    if (!link || !param) {
+        return null;
+    }
+
+    const params = getHash(link).split('&');
+
+    for (let i = 0; i < params.length; i++) {
+        const [key, value] = params[i].split('=');
+
+        if (key === param) {
+            return value;
+        }
+    }
+
+    return null;
+};
