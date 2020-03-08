85556f382fcc41872ba4f1c50e4aa2f06c57da59
Nikita Barinov <nikitabarinov2301@gmail.com>
2020-03-09 02:15:21 +0400

    add Headline component

---
+++ src/components/Headline/Headline.tsx
@@ -0,0 +1,17 @@
+import { FC, HTMLAttributes, createElement, memo } from 'react';
+import cn from 'classnames';
+
+import { HasChildren } from '../../types/props';
+
+export interface HeadlineProps extends HTMLAttributes<HTMLHeadingElement>, HasChildren {
+    level?: 1 | 2 | 3 | 4 | 5 | 6;
+}
+
+const Headline: FC<HeadlineProps> = ({ className, level = 1, children, ...restProps }: HeadlineProps) =>
+    createElement(
+        `h${level}`,
+        { className: cn(className, 'Headline', 'padding-blue--rl'), ...restProps },
+        children
+    );
+
+export default memo(Headline);
