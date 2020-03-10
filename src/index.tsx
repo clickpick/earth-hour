e8c3897b9ed99ef5e3b2bc02ce4accf8bc9e6156
Nikita Barinov <nikitabarinov2301@gmail.com>
2020-03-10 20:57:05 +0400

    add VKWebAppInit in index

--- src/index.tsx
+++ src/index.tsx
@@ -4,8 +4,12 @@ import 'core-js/features/set';
 import React from 'react';
 import { render } from 'react-dom';
 
+import bridge from '@vkontakte/vk-bridge';
+
 import configureStore from './store/configureStore';
 import Root from './containers/Root';
+
+bridge.send('VKWebAppInit');
 
 const store = configureStore();
 
