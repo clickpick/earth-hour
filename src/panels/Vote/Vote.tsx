9c7a13912c05d251e5025e62ba4e93649a4116c0
Nikita Barinov <nikitabarinov2301@gmail.com>
2020-03-14 11:39:13 +0400

    change Vote panel

--- src/panels/Vote/Vote.tsx
+++ src/panels/Vote/Vote.tsx
@@ -1,6 +1,9 @@
-import React, { FC } from 'react';
+import React, { FC, useMemo, useEffect } from 'react';
+
+import useVote from '../../hooks/use-vote';
 
 import { Panel, PanelHeaderSimple, PanelHeaderBack } from '@vkontakte/vkui';
+import Question from '../../components/Question';
 
 export interface VoteProps {
     id: string,
@@ -8,12 +11,28 @@ export interface VoteProps {
 }
 
 const Vote: FC<VoteProps> = ({ id, goBack }: VoteProps) => {
+    const { questionIds, questions, nextQuestionId, setNextQuestionId, attachAnswer } = useVote();
+
+    useEffect(() => () => { setNextQuestionId(); }, [setNextQuestionId]);
+
+    const bodyView = useMemo(() => (!questionIds)
+        ? <h1>А где вопросы?</h1>
+        : <Question
+            className="padding-blue"
+            {...questions[nextQuestionId]}
+            currentQuestionNumber={questionIds.indexOf(nextQuestionId) + 1}
+            questionsCount={questionIds.length}
+            goNext={setNextQuestionId}
+            attachAnswer={attachAnswer} />,
+        [questionIds, questions, nextQuestionId, setNextQuestionId, attachAnswer]);
+
     return (
-        <Panel id={id}>
+        <Panel id={id} separator={false}>
             <PanelHeaderSimple
                 left={<PanelHeaderBack onClick={goBack} />}
-                separator={false} />
-            Vote
+                separator={false}
+                children="Час Земли" />
+            {bodyView}
         </Panel>
     );
 };
