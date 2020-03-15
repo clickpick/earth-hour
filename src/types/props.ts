a60935d36f4356b5df6378e5d61d496df3474721
Nikita Barinov <nikitabarinov2301@gmail.com>
2020-03-15 18:04:50 +0400

    add Story & StoryItem interfaces

--- src/types/props.ts
+++ src/types/props.ts
@@ -3,3 +3,24 @@ import { ReactNode } from 'react';
 export interface HasChildren {
     children?: ReactNode
 }
+
+export interface Story {
+    id: string,
+    name: string,
+    photo: string,
+    seen: boolean,
+    lastUpdated: number
+    items: Array<StoryItem>
+}
+
+export interface StoryItem {
+    id: string,
+    length: number
+    link: string | null,
+    linkText: string | boolean
+    preview: string
+    seen: boolean
+    src: string
+    time: number
+    type: 'photo' | 'video'
+}
