5a824121dfa265e24b943dcbab8986fe71bfcf8b
Nikita Barinov <nikitabarinov2301@gmail.com>
2020-03-15 18:06:41 +0400

    change StoryItem component

--- src/components/StoryItem/StoryItem.tsx
+++ src/components/StoryItem/StoryItem.tsx
@@ -6,29 +6,40 @@ import { HasChildren } from '../../types/props';
 import Caption from '../Caption';
 
 export interface StoryItemProps extends HTMLAttributes<HTMLButtonElement>, HasChildren {
-    preview: string,
-    gradient?: 'red-orange' | 'orange-yellow' | 'green-yellow' | 'blue-green' | 'purple-aqua'
+    seen?: boolean,
+    gradient?: 'red-orange' | 'orange-yellow' | 'green-yellow' | 'blue-green' | 'purple-aqua',
+    photo: string,
+    name: string,
+    lastUpdated: number
 }
 
-const StoryItem: FC<StoryItemProps> = ({ className, gradient, preview, children, ...restProps }: StoryItemProps) => {
+const StoryItem: FC<StoryItemProps> = ({ className, gradient, seen, photo, name, lastUpdated, children, ...restProps }: StoryItemProps) => {
     const classNames = useMemo(() =>
-        cn(className, 'StoryItem', 'padding-yellow', `StoryItem--${gradient}`),
-        [className, gradient]);
+        cn(className, 'StoryItem', 'padding-yellow', 'story', `StoryItem--${gradient}`, {
+            'StoryItem--seen': seen
+        }),
+        [className, gradient, seen]);
 
-    const previewView = useMemo(() =>
+    const photoView = useMemo(() =>
         <div
-            className="StoryItem__preview"
-            style={{ backgroundImage: `url(${preview})` }} />,
-        [preview]);
-
-    const contentView = useMemo(() => (!!children) &&
-        <Caption className="margin-aqua--top color-opacity--secondary" children={children} />,
-        [children]);
+            className="StoryItem__preview item-preview"
+            style={{ backgroundImage: `url(${photo})` }}>
+            <img src={photo} alt="" />
+        </div>,
+        [photo]);
 
     return (
         <button className={classNames} {...restProps}>
-            <div className="StoryItem__wrapper" children={previewView} />
-            {contentView}
+            <div className="item-link StoryItem__wrapper">
+                <div className="StoryItem__photo-wrapper margin-aqua--bottom" children={photoView} />
+                <div className="info StoryItem__info" itemProp="author" itemType="http://schema.org/Person">
+                    <Caption
+                        className="name color-opacity--secondary StoryItem__name"
+                        itemProp="name"
+                        children={name} />
+                    <span className="time StoryItem__time">{lastUpdated}</span>
+                </div>
+            </div>
         </button>
     );
 };
