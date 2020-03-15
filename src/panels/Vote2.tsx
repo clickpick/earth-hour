d616c23e66a12532a0d21b1bcc706cdb95c9397a
Nikita Barinov <nikitabarinov2301@gmail.com>
2020-03-15 20:42:06 +0400

    change Vote2 panel

--- src/panels/Vote2.tsx
+++ src/panels/Vote2.tsx
@@ -1,8 +1,11 @@
 import React, { FC, useCallback } from 'react';
 
+import useUser from '../hooks/use-user';
 import bridge from '@vkontakte/vk-bridge';
 
 import { Panel, PanelHeaderSimple, PanelHeaderBack } from '@vkontakte/vkui';
+import Planet from '../components/Planet';
+import Transition from '../components/Transition';
 import Group from '../components/Group';
 import Button from '../components/Button';
 import About from '../components/About';
@@ -15,21 +18,38 @@ export interface VoteProps {
 }
 
 const Vote2: FC<VoteProps> = ({ id, goBack }: VoteProps) => {
-    const allowNotification = useCallback(() => {
-        bridge.send('VKWebAppAllowNotifications', {});
-    }, []);
+    const { pending, data, toggleNotifications } = useUser();
+
+    const allowNotification = useCallback(async () => {
+        try {
+            const response = await bridge.sendPromise('VKWebAppAllowNotifications');
+
+            if (response.result) {
+                toggleNotifications(true);
+            }
+        } catch (e) { }
+    }, [toggleNotifications]);
 
     return (
         <Panel id={id} separator={false}>
             <PanelHeaderSimple left={<PanelHeaderBack onClick={goBack} />} separator={false} />
-            <Group className="margin-pink--bottom" vertical center>
-                <Button
-                    shape="circle"
-                    icon={<IconNotification />}
-                    onClick={allowNotification}>
-                    Уведомить, когда<br />квиз станет доступным
+
+            <Planet
+                className="margin-pink--bottom padding-blue--rl"
+                mood="lock"
+                title="Этот квиз будет доступен 28 марта"
+                message="После его прохождения, ты сможешь получить подарок от WWF." />
+            <Transition in={!data?.notificationsAreEnabled} mountOnEnter>
+                <Group className="margin-pink--bottom" vertical center>
+                    <Button
+                        shape="circle"
+                        icon={<IconNotification />}
+                        onClick={allowNotification}
+                        disabled={pending}>
+                        Уведомить, когда<br />квиз станет доступным
                 </Button>
-            </Group>
+                </Group>
+            </Transition>
             <About />
         </Panel>
     );
