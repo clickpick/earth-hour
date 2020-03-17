def3b4b09bfaee65fa4f558fe2bad050000a514f
Nikita Barinov <nikitabarinov2301@gmail.com>
2020-03-17 15:37:59 +0400

    add share vk helper

--- src/helpers/vk.ts
+++ src/helpers/vk.ts
@@ -1,5 +1,6 @@
 import bridge from '@vkontakte/vk-bridge';
 import { Links } from '../config';
 
-export const shareApp = () => bridge.send('VKWebAppShare', { link: Links.APP_LINK });
+export const share = (link: string) => bridge.send('VKWebAppShare', { link });
+export const shareApp = () => share(Links.APP_LINK);
 
