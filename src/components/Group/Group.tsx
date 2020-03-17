b2f375ea94c3210747af99110cdd0826a775cbd9
Nikita Barinov <nikitabarinov2301@gmail.com>
2020-03-17 15:46:18 +0400

    add jcCenter prop in Group component

--- src/components/Group/Group.tsx
+++ src/components/Group/Group.tsx
@@ -5,18 +5,20 @@ import { HasChildren } from '../../types/props';
 
 export interface GroupProps extends HTMLAttributes<HTMLDivElement>, HasChildren {
     vertical?: boolean,
+    jcCenter?: boolean,
     center?: boolean,
     start?: boolean,
     end?: boolean,
 }
 
-const Group: FC<GroupProps> = ({ className, vertical, center, start, end, ...restProps }: GroupProps) => {
+const Group: FC<GroupProps> = ({ className, vertical, jcCenter, center, start, end, ...restProps }: GroupProps) => {
     const classNames = useMemo(() => cn(className, 'Group', {
         'Group--vertical': vertical,
+        'Group--jc-center': jcCenter,
         'Group--center': center,
         'Group--start': start,
         'Group--end': end
-    }), [className, vertical, center, start, end]);
+    }), [className, vertical, jcCenter, center, start, end]);
 
     return <div className={classNames} {...restProps} />;
 };
