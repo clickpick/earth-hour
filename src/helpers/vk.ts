3681ec5f71b28366791405c17e391531e053edf7
Nikita Barinov <nikitabarinov2301@gmail.com>
2020-03-17 15:43:26 +0400

    add showStoryBox vk helper

--- src/helpers/vk.ts
+++ src/helpers/vk.ts
@@ -4,3 +4,21 @@ import { Links } from '../config';
 export const share = (link: string) => bridge.send('VKWebAppShare', { link });
 export const shareApp = () => share(Links.APP_LINK);
 
+export const showStoryBox = (stickerUrl: string) =>
+    bridge.send('VKWebAppShowStoryBox', {
+        background_type: 'none',
+        stickers: [
+            {
+                sticker_type: 'renderable',
+                sticker: {
+                    content_type: 'image',
+                    url: stickerUrl,
+                    transform: {
+                        translation_y: 0.08,
+                        relation_width: 0.6,
+                        gravity: 'right_top'
+                    },
+                }
+            }
+        ]
+    });
