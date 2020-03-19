42b75423850f71b10c669062295df099d2c1f5f5
Nikita Barinov <nikitabarinov2301@gmail.com>
2020-03-19 15:14:26 +0400

    add Quiz component

---
+++ src/components/Quiz/Quiz.tsx
@@ -0,0 +1,162 @@
+import React, { FC, useRef, useMemo, useCallback, useEffect, memo } from 'react';
+
+import { Questions } from '../../types/store';
+
+// @ts-ignore
+import { checkWebPSupport } from 'supports-webp-sync';
+import { resultProps } from '../../config';
+import { shareApp, showStoryBox } from '../../helpers/vk';
+
+import Question from '../Question';
+import Planet from '../Planet';
+import Transition from '../Transition';
+import Group from '../Group';
+import Button from '../Button';
+import WWFPrize from '../WWFPrize';
+
+import { ReactComponent as IconReply } from '../../svg/reply.svg';
+import { ReactComponent as IconAgain } from '../../svg/again.svg';
+import { ReactComponent as IconnUnion } from '../../svg/union.svg';
+import { ReactComponent as IconHome } from '../../svg/home.svg';
+
+const supportWebp: boolean = checkWebPSupport();
+
+export interface QuizProps {
+    isRightAnswersCount: number,
+    questionIds: Array<number>,
+    questions: Questions,
+    nextQuestionId: number | null,
+    storyLink: string,
+    answersCount: number,
+    finish: boolean,
+    setNextQuestionId(): void,
+    setIsRightAnswersCount(count: number): void,
+    attachAnswer(questionId: number, answerId: number): void,
+    resetQuiz(): void,
+    exit(): void
+}
+
+const Quiz: FC<QuizProps> = ({
+    isRightAnswersCount, questionIds, questions, nextQuestionId,
+    storyLink, answersCount, finish,
+    setNextQuestionId, setIsRightAnswersCount, attachAnswer, resetQuiz, exit
+}: QuizProps) => {
+    const isRight = useRef<number>(isRightAnswersCount);
+
+    const questionsCount = useMemo<number>(() => (Array.isArray(questionIds) && questionIds.length) || 0, [questionIds]);
+    const shareStory = useCallback(() => showStoryBox(storyLink), [storyLink]);
+
+    const incrementIsRight = useCallback((count: number) => {
+        isRight.current += count;
+    }, []);
+
+    useEffect(() => () => {
+        if (!finish) {
+            setNextQuestionId();
+            setIsRightAnswersCount(isRight.current);
+        }
+    }, [finish, setNextQuestionId, setIsRightAnswersCount]);
+
+    const maskView = useMemo(() => {
+        if (nextQuestionId === null) {
+            return null;
+        }
+
+        const { image } = questions[nextQuestionId];
+
+        return (
+            <div
+                className="Quiz__mask"
+                style={{
+                    backgroundImage: `
+                        linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
+                        url(${(supportWebp) ? image.webp.x1 : image.jpg.x1})
+                    `,
+                }} />
+        );
+    }, [questions, nextQuestionId]);
+
+    const questionView = useCallback((id, index) => {
+        if (nextQuestionId !== id) {
+            return null;
+        }
+
+        return (
+            <Question
+                key={id}
+                className="padding-blue"
+                {...questions[id]}
+                currentQuestionNumber={index + 1}
+                questionsCount={questionsCount}
+                triggerAnswer={incrementIsRight}
+                goNext={setNextQuestionId}
+                attachAnswer={attachAnswer} />
+        );
+    }, [nextQuestionId, questions, questionsCount, incrementIsRight, setNextQuestionId, attachAnswer]);
+
+    const resultView = useMemo(() => {
+        const result = (isRight.current / answersCount) * 100 || 0;
+        const showPrize = finish || result === 100;
+        let mood: 'best' | 'good' | 'bad' | 'lock' =
+            (showPrize)
+                ? 'best'
+                : (result >= 60 && result <= 80)
+                    ? 'good'
+                    : 'bad';
+
+        return (
+            <Transition in={true} timeout={100}>
+                <Planet
+                    className="margin-pink--bottom padding-blue--rl"
+                    mood={mood}
+                    {...resultProps[mood]} />
+                <Group jcCenter>
+                    <Button
+                        className="margin-purple--right"
+                        shape="circle"
+                        icon={<IconReply />}
+                        onClick={shareApp}>
+                        Поделиться<br />приложением
+                    </Button>
+                    {(showPrize)
+                        ? <Button
+                            className="margin-purple--right"
+                            shape="circle"
+                            icon={<IconnUnion />}
+                            onClick={shareStory}>
+                            Поделиться<br />в сторис
+                        </Button>
+                        : <Button
+                            className="margin-purple--right"
+                            shape="circle"
+                            icon={<IconAgain />}
+                            onClick={resetQuiz}>
+                            Повторить квиз<br />заново
+                        </Button>}
+
+                    <Button
+                        shape="circle"
+                        icon={<IconHome />}
+                        onClick={exit}>
+                        Вернуться<br />на главную
+                    </Button>
+                </Group>
+                {(showPrize) && <WWFPrize className="margin-pink--top" />}
+            </Transition>
+        );
+    }, [finish, answersCount, shareStory, resetQuiz, exit]);
+
+    const bodyView = useMemo(() => (nextQuestionId !== null)
+        ? questionIds.map(questionView)
+        : resultView,
+        [nextQuestionId, questionIds, questionView, resultView]);
+
+    return (
+        <>
+            {maskView}
+            {bodyView}
+        </>
+    );
+};
+
+export default memo(Quiz);
--- src/panels/Vote.tsx
+++ src/panels/Vote.tsx
@@ -1,31 +1,12 @@
-import React, { FC, useRef, useCallback, useMemo, useEffect } from 'react';
-
-import './Vote.css';
-
-// @ts-ignore
-import { checkWebPSupport } from 'supports-webp-sync';
+import React, { FC, useEffect, useMemo } from 'react';
 
 import { PanelSecondary } from '../types/props';
 import useVote from '../hooks/use-vote';
-import { resultProps } from '../config';
-import { shareApp, showStoryBox } from '../helpers/vk';
 
 import { Panel, PanelHeaderSimple, PanelHeaderBack } from '@vkontakte/vkui';
-import Question from '../components/Question';
-import Planet from '../components/Planet';
-import Transition from '../components/Transition';
-import Group from '../components/Group';
-import Button from '../components/Button';
-import WWFPrize from '../components/WWFPrize';
-
-import { ReactComponent as IconReply } from '../svg/reply.svg';
-import { ReactComponent as IconAgain } from '../svg/again.svg';
-import { ReactComponent as IconnUnion } from '../svg/union.svg';
-import { ReactComponent as IconHome } from '../svg/home.svg';
+import Quiz from '../components/Quiz';
 
 export interface VoteProps extends PanelSecondary { }
-
-const supportWebp: boolean = checkWebPSupport();
 
 const Vote: FC<VoteProps> = ({ id, goBack }: VoteProps) => {
     const {
@@ -35,129 +16,35 @@ const Vote: FC<VoteProps> = ({ id, goBack }: VoteProps) => {
         sendAnswers, resetQuiz
     } = useVote();
 
-    const isRight = useRef<number>(isRightAnswersCount);
-
-    useEffect(() => () => {
-        if (!finish) {
-            setNextQuestionId();
-            setIsRightAnswersCount(isRight.current);
-        }
-    }, [finish, setNextQuestionId, setIsRightAnswersCount]);
-
     useEffect(() => {
         if (!finish && answers.length === questionIds?.length && nextQuestionId === null) {
             setTimeout(() => { sendAnswers(answers); }, 1500);
         }
     }, [finish, answers, questionIds, nextQuestionId, sendAnswers]);
 
-    const questionsCount = useMemo(() => (Array.isArray(questionIds) && questionIds.length) || 0, [questionIds]);
-
-    const incrementIsRight = useCallback((count: number) => {
-        isRight.current += count;
-    }, []);
-
-    const shareStory = useCallback(() => showStoryBox(storyLink as string), [storyLink]);
-
-    const questionView = useCallback((id, index) => {
-        if (nextQuestionId !== id) {
-            return null;
-        }
-
-        return (
-            <Question
-                key={id}
-                className="padding-blue"
-                {...questions[id]}
-                currentQuestionNumber={index + 1}
-                questionsCount={questionsCount}
-                triggerAnswer={incrementIsRight}
-                goNext={setNextQuestionId}
-                attachAnswer={attachAnswer} />
-        );
-    }, [nextQuestionId, questions, questionsCount, incrementIsRight, setNextQuestionId, attachAnswer]);
-
-    const resultView = useMemo(() => {
-        const result = (isRight.current / answers.length) * 100 || 0;
-        const showPrize = finish || result === 100;
-        let mood: 'best' | 'good' | 'bad' | 'lock' =
-            (showPrize)
-                ? 'best'
-                : (result >= 60 && result <= 80)
-                    ? 'good'
-                    : 'bad';
-
-        return (
-            <Transition in={true} timeout={100}>
-                <Planet
-                    className="margin-pink--bottom padding-blue--rl"
-                    mood={mood}
-                    {...resultProps[mood]} />
-                <Group jcCenter>
-                    <Button
-                        className="margin-purple--right"
-                        shape="circle"
-                        icon={<IconReply />}
-                        onClick={shareApp}>
-                        Поделиться<br />приложением
-                    </Button>
-                    {(showPrize)
-                        ? <Button
-                            className="margin-purple--right"
-                            shape="circle"
-                            icon={<IconnUnion />}
-                            onClick={shareStory}>
-                            Поделиться<br />в сторис
-                        </Button>
-                        : <Button
-                            className="margin-purple--right"
-                            shape="circle"
-                            icon={<IconAgain />}
-                            onClick={resetQuiz}>
-                            Повторить квиз<br />заново
-                        </Button>}
-
-                    <Button
-                        shape="circle"
-                        icon={<IconHome />}
-                        onClick={goBack}>
-                        Вернуться<br />на главную
-                    </Button>
-                </Group>
-                {(showPrize) && <WWFPrize className="margin-pink--top" />}
-            </Transition>
-        );
-    }, [finish, answers, shareStory, resetQuiz, goBack]);
-
-    const maskView = useMemo(() => {
-        if (nextQuestionId === null) {
-            return null;
-        }
-
-        const { image } = questions[nextQuestionId];
-
-        return (
-            <div
-                className="Vote__mask"
-                style={{
-                    backgroundImage: `
-                        linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 1)),
-                        url(${(supportWebp) ? image.webp.x1 : image.jpg.x1})
-                    `,
-                }} />
-        );
-    }, [questions, nextQuestionId])
-
-    const bodyView = useMemo(() => (!questionIds)
+    const bodyView = useMemo(() => (questionIds === null)
         ? <h1>А где вопросы?</h1>
-        : (nextQuestionId !== null)
-            ? questionIds.map(questionView)
-            : resultView,
-        [nextQuestionId, questionIds, questionView, resultView]);
+        : <Quiz
+            isRightAnswersCount={isRightAnswersCount}
+            questionIds={questionIds}
+            questions={questions}
+            nextQuestionId={nextQuestionId}
+            storyLink={storyLink as string}
+            answersCount={answers.length}
+            finish={finish}
+            setNextQuestionId={setNextQuestionId}
+            setIsRightAnswersCount={setIsRightAnswersCount}
+            attachAnswer={attachAnswer}
+            resetQuiz={resetQuiz}
+            exit={goBack} />,
+        [
+            isRightAnswersCount, questionIds, questions, nextQuestionId, storyLink, answers, finish,
+            setNextQuestionId, setIsRightAnswersCount, attachAnswer, resetQuiz, goBack
+        ]);
 
     return (
         <Panel id={id} separator={false}>
             <PanelHeaderSimple left={<PanelHeaderBack onClick={goBack} />} separator={false} />
-            {maskView}
             {bodyView}
         </Panel>
     );
