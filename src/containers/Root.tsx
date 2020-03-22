109c3ee99f932c910ad2f888b5e9fdf02ce1e83d
Nikita Barinov <nikitabarinov2301@gmail.com>
2020-03-22 18:02:47 +0400

    use Offline component in Root container

--- src/containers/Root.tsx
+++ src/containers/Root.tsx
@@ -1,15 +1,31 @@
-import React, { FC } from 'react';
+import React, { FC, useState, useEffect } from 'react';
 import { Provider } from 'react-redux';
 
 import App from './App';
+import Offline from '../components/Offline';
 
 export interface RootProps {
     store: any
 }
 
-const Root: FC<RootProps> = ({ store }: RootProps) =>
-    <Provider store={store}>
-        <App />
-    </Provider>;
+const Root: FC<RootProps> = ({ store }: RootProps) => {
+    const [showOffline, setShowOffline] = useState<boolean>(false);
+
+    useEffect(() => {
+        function handleOnlineStatus() {
+            setShowOffline(!navigator.onLine);
+        }
+
+        window.addEventListener('online', handleOnlineStatus);
+        window.addEventListener('offline', handleOnlineStatus);
+    }, []);
+
+    return (
+        <Provider store={store}>
+            <App />
+            <Offline visible={showOffline} />
+        </Provider>
+    );
+};
 
 export default Root;
