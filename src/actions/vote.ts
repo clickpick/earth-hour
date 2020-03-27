46f53d44332bd525d95fb38b38de5c87e7864280
Nikita Barinov <nikitabarinov2301@gmail.com>
2020-03-27 20:16:42 +0300

    vote id 1 -> 2

--- src/actions/vote.ts
+++ src/actions/vote.ts
@@ -5,7 +5,7 @@ import { vote } from '../schema';
 export const fetchQuetions = () => ({
     [CALL_API]: {
         types: [ActionTypes.VOTE_REQUEST, ActionTypes.VOTE_SUCCESS, ActionTypes.VOTE_FAILURE],
-        endpoint: '/votes/1',
+        endpoint: '/votes/2',
         method: Methods.GET,
         schema: vote
     }
